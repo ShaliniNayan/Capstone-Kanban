72975d406935a506a94e08e0b9df800cb57ca441
mohametalmeari <mohamet.almeari@gmail.com>
2023-05-23 17:19:50 +0300

    Create Comments Popup Window

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './style.css';
+import previewImg from './images/pokemon-preview.png';
+const commentsPopup = document.getElementById('comments-popup');
+const popupImg = document.querySelector('.popup-img');
+popupImg.src =previewImg;
+
+
